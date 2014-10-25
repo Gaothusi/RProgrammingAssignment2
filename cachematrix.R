40abeb4e789b8a1676a764ffe8d6c2134b110fd8
Wabuya Gaothusi <Gao2c@Wabuyas-iMac.local>
2014-10-25 00:06:26 -0600

    Commiting my changes for assignment 2.Cache matrix is compiling flawlessly!!!

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Caching the Inverse of a Matrix  so that when we need it again,
+## it can be looked up in the cache rather than recomputed.
+
+makeCacheMatrix <- function(x = matrix()) {
+        mtrx<-NULL
+        set<-function(y){
+                x<<-y
+                mtrx<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) mtrx <<- solve
+        getmatrix<-function() mtrx
+        list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
+}
+
+
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then this function will retrieve 
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inversemtrx <- x$getmatrix()
+        if(!is.null(inversemtrx)){
+                message("getting cached data")
+                return(inversemtrx)
+        }
+        matrix <- x$get()
+        inversemtrx <- solve(matrix, ...)
+        x$setmatrix(inversemtrx)
+        inversemtrx
+}
